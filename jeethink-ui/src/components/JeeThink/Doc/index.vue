5a0675f4c7f23cefa30a03c41972cb63bda792e3
ss <1991995430@qq.com>
2021-07-11 20:50:53 +0800

    前端代码提交

---
+++ jeethink-ui/src/components/JeeThink/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="link" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JeeThinkDoc',
+  data() {
+    return {
+      url: 'https://item.taobao.com/item.htm?id=616173515805'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
---
+++ jeethink-ui/src/components/JeeThink/Site/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="guide" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JeeThinkSite',
+  data() {
+    return {
+      url: 'http://jeethink.vip'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
